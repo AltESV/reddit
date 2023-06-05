b7775b5342e561c8fd6512a8fd777a75c8ca0ce5
AltESV <ishigami.daniel@gmail.com>
2023-06-05 14:53:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#Reddit Bot
+This is a moderator bot using https://www.npmjs.com/package/snoostorm
+
+Go to https://www.reddit.com/prefs/apps and hit "create app"
+
+Configure credentials 
+
+Run & Deploy bot
---
+++ bot.js
@@ -0,0 +1,24 @@
+const Snoowrap = require('snoowrap');
+const { CommentStream } = require('snoostorm');
+const creds = require('./credentials.json');
+
+const client = new Snoowrap(creds);
+
+const BOT_START = Date.now() / 1000;
+
+const comments = new CommentStream(client, { 
+    subreddit: 'fanaverse', 
+    limit: 10, 
+    pollTime: 10000 
+});
+
+comments.on('item', (item) => {
+    console.log(item);
+});
+
+//Creates an infinite loop
+// comments.on('item', (item) => {
+//     if(item.created_utc < BOT_START) return;
+
+//     item.reply('Welcome to the FANAVERSE my friend!');
+// });
